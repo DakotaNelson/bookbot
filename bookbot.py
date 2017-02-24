3648b6873adcb006dcaaa6db52828bdf453f777e
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2017-02-23 20:46:05 -0500

    more responses, better response formatting, some code cleanup

--- bookbot.py
+++ bookbot.py
@@ -1,5 +1,8 @@
 import os
 import time
+import random
+import string
+
 from slackclient import SlackClient
 
 import urllib.parse
@@ -60,11 +63,10 @@ print(BOT_ID)
 
 # constants
 AT_BOT = "<@" + BOT_ID + ">"
-EXAMPLE_COMMAND = "on" # or "about"
+COMMANDS = ["on", "about"]
 
 # instantiate Slack & Twilio clients
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
-print(slack_client)
 
 def handle_command(command, channel):
     """
@@ -72,60 +74,156 @@ def handle_command(command, channel):
         are valid commands. If so, then acts on the commands. If not,
         returns back what it needs for clarification.
     """
-    response = "Not sure what you mean. Try something like '@bookbot, find me a book on programming'"
+    error_msgs = [
+        "Not sure what you mean.",
+        "I didn't catch that.",
+        "Sorry, I'm confused.",
+        "What was that?",
+        "Huh?",
+        "What?"
+    ]
 
-    if EXAMPLE_COMMAND in command:
-        print("command: {}".format(command))
-        query = command.split('on')[-1].strip()
-        # TODO remove punctuation
-        print("query: {}".format(query))
-        results = search_tind(query)
-        attachments = []
+    joiners = [
+        "Try something like",
+        "Perhaps try",
+        "I'm looking for things like"
+    ]
 
-        # only ever show 5 results
-        if len(results) > 5:
-            results = results[:5]
+    examples = [
+        "find me a book",
+        "I want something",
+        "do you have anything",
+        "could you find me something",
+        "what about some stuff",
+        "I'm looking for books",
+        "could you do me a favor and refer me to some fine reading material",
+        "can you get me something",
+        "do you have any books",
+        "I want a book",
+    ]
 
-        for result in results:
-            author = result['author']
-            title = result['title']
-            link = result['link']
-            desc = result['description']
-            attachment = {
-                "fallback": "{}, by {}".format(title, author),
-                "color": "#36a64f",
-                # "pretext": "",
-                # "author_name": author,
-                "title": title,
-                "title_link": link,
-                "text": desc,
-                #"footer": "Olin Bookbot"
-            }
-            attachments.append(attachment)
+    queries = [
+        "programming",
+        "engineering",
+        "cats",
+        "dogs",
+        "trains",
+        "information security",
+        "materials science",
+        "chemistry",
+        "space manufacturing",
+        "audio production",
+        "chatbots",
+        "library science",
+        "welding",
+        "musical theory",
+        "the flight speed of unladen swallows"
+    ]
 
-        slack_client.api_call("chat.postMessage",
-                channel=channel,
-                text="Here are a few!",
-                attachments=attachments,
-                as_user=True)
+    error = random.choice(error_msgs)
+    joiner = random.choice(joiners)
+    keyword = random.choice(COMMANDS)
+    example = random.choice(examples)
+    query = random.choice(queries)
 
+    response = "{} {} '@bookbot, {} {} {}'".format(
+        error,
+        joiner,
+        example,
+        keyword,
+        query
+    )
+
+    # determine if the user used any of our keywords
+    tokens = command.split(' ')
+    print("tokens:")
+    print(tokens)
+    keyword_locations = []
+    for c in COMMANDS:
+        try:
+            keyword_locations.append(tokens.index(c))
+        except ValueError:
+            pass
+
+    try:
+        keyword_location = min(keyword_locations)
+    except ValueError:
+        # no keywords
+
+        slack_client.api_call("chat.postMessage", channel=channel,
+                              text=response, as_user=True)
         return
 
-    elif "events" in command:
-        now = datetime.datetime.now()
-        events = interact_with_database("select * from events where event_start between \'%s-%s-%s\' and \'%s-%s-%s\' order by event_start asc" #timestamps in postgres are 'YYYY-MM-DD HH-MM' 
-                                %(str(now.year), str(now.month), str(now.day),
-                                str(now.year), str(now.month), str(int(now.day)+1)), debug = False)
-        if events:
-            for event in events:
-                response = "Event: {}, Location: {}, Duration: {} to {}, Description: {}, Link: {}".format(event[1], event[5], event[3].time(), event[4].time(), event[6], event[7])
-                slack_client.api_call("chat.postMessage", channel=channel,
-                      text=response, as_user=True)
-        else:
-            response = "No events found!" 
+    # elif "events" in command:
+    #     now = datetime.datetime.now()
+    #     events = interact_with_database("select * from events where event_start between \'%s-%s-%s\' and \'%s-%s-%s\' order by event_start asc" #timestamps in postgres are 'YYYY-MM-DD HH-MM'
+    #                             %(str(now.year), str(now.month), str(now.day),
+    #                             str(now.year), str(now.month), str(int(now.day)+1)), debug = False)
+    #     if events:
+    #         for event in events:
+    #             response = "Event: {}, Location: {}, Duration: {} to {}, Description: {}, Link: {}".format(event[1], event[5], event[3].time(), event[4].time(), event[6], event[7])
+    #             slack_client.api_call("chat.postMessage", channel=channel,
+    #                   text=response, as_user=True)
+    #     else:
+    #         response = "No events found!"
+    #
+    # slack_client.api_call("chat.postMessage", channel=channel,
+    #                       text=response, as_user=True)
 
-    slack_client.api_call("chat.postMessage", channel=channel,
-                          text=response, as_user=True)
+    print("command: {}".format(command))
+
+    # parse the query from the command
+    query = ' '.join(tokens[keyword_location+1:])
+    print("query before cleaning: {}".format(query))
+    for ch in string.punctuation:
+        # remove punctuation
+        query = query.replace(ch, "")
+    # remove whitespace
+    query = query.strip()
+    print("query: {}".format(query))
+
+    results = search_tind(query)
+    # TODO what if no search results
+    attachments = []
+
+    # only ever show 5 results
+    if len(results) > 5:
+        results = results[:5]
+
+    for result in results:
+        author = result['author']
+        title = result['title']
+        link = result['link']
+        desc = result['description']
+        # TODO add some padding so one result with a description doesn't
+        # overwhelm the rest
+        attachment = {
+            "fallback": "{}, by {}".format(title, author),
+            "color": "#36a64f",
+            # "pretext": "",
+            # "author_name": author,
+            "title": title,
+            "title_link": link,
+            "text": desc,
+            "footer": "Olin Bookbot"
+        }
+        attachments.append(attachment)
+
+    response_choices = [
+        "Here are a few!",
+        "How about these?",
+        "Hope this helps!",
+        "I found some:",
+        "What about these?",
+        "I dug up a few:",
+        "Maybe these will help:",
+        "No problem!"
+    ]
+    slack_client.api_call("chat.postMessage",
+            channel=channel,
+            text=random.choice(response_choices),
+            attachments=attachments,
+            as_user=True)
 
 def parse_slack_output(slack_rtm_output):
     """
