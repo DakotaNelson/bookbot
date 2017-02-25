e6b76f4b959d9982e7d193a458e56d1e22ced97c
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2017-02-25 16:38:04 -0500

    remove events from global commands

--- bookbot.py
+++ bookbot.py
@@ -61,7 +61,7 @@ BOT_ID = os.environ.get("BOT_ID")
 
 # constants
 AT_BOT = "<@" + BOT_ID + ">"
-COMMANDS = ["on", "about", "events"]
+COMMANDS = ["on", "about"]
 
 # instantiate Slack & Twilio clients
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
